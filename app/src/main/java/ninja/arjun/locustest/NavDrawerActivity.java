a84ed6660e3443cdd4636a8c0b37b3f603c23ad4
Arjun Munji <arjunmunji@gmail.com>
2016-01-31 20:45:00 +0530

    :confetti_ball:  :confetti_ball: User details appear in Navigation Drawer

--- app/src/main/java/ninja/arjun/locustest/NavDrawerActivity.java
+++ app/src/main/java/ninja/arjun/locustest/NavDrawerActivity.java
@@ -1,10 +1,23 @@
 package ninja.arjun.locustest;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.Gravity;
@@ -17,8 +30,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class NavDrawerActivity extends AppCompatActivity
@@ -28,7 +50,9 @@ public class NavDrawerActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_nav_drawer);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -51,15 +75,19 @@ public class NavDrawerActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
-        String name = sharedPreferences.getString("name", null);
-        String email = sharedPreferences.getString("email", null);
-        String photoURI = sharedPreferences.getString("photoURI", null);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-//        setContentView(R.layout.activity_nav_drawer);
-//        TextView NameTextView = (TextView) findViewById(R.id.NameTextView);
-//        String loginSuccessString = getString(R.string.loginFailMessage);
-//        NameTextView.setText(name);
+
+
+
+//        Toast toast = Toast.makeText(getApplicationContext(),
+//                name,
+//                Toast.LENGTH_LONG);
+//        toast.setGravity(Gravity.BOTTOM, 0, 10);
+//        toast.show();
+
 //        NameTextView.setText();
 
     }
@@ -78,7 +106,38 @@ public class NavDrawerActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("name", null);
+        String email = sharedPreferences.getString("email", null);
+        Uri photoURI = Uri.parse(sharedPreferences.getString("photoURI", null));
+
+//        try {
+//            Bitmap userBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
+//
+//            ImageView userPhotoImageView = (ImageView) findViewById(R.id.userPhotoImageView);
+//            userPhotoImageView.setImageBitmap(userBitmap);
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        ImageView userPhotoImageView = (ImageView) findViewById(R.id.userPhotoImageView);
+        String url = photoURI.toString();
+        BitmapFactory.Options bmOptions;
+        bmOptions = new BitmapFactory.Options();
+        bmOptions.inSampleSize = 1;
+        Bitmap bm = loadBitmap(url, bmOptions);
+        bm = Bitmap.createScaledBitmap(bm, 120, 120, false);
+        userPhotoImageView.setImageBitmap(getCircleBitmap(bm));
+
+
+
+
+        TextView NameTextView = (TextView) findViewById(R.id.NameTextView);
+        NameTextView.setText(name);
+        TextView EmailTextView = (TextView) findViewById(R.id.EmailTextView);
+        EmailTextView.setText(email);
         return true;
     }
 
@@ -103,22 +162,76 @@ public class NavDrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public static Bitmap loadBitmap(String URL, BitmapFactory.Options options) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in, null, options);
+            in.close();
+        } catch (IOException e1) {
+        }
+        return bitmap;
+    }
+
+    private static InputStream OpenHttpConnection(String strURL)
+            throws IOException {
+        InputStream inputStream = null;
+        URL url = new URL(strURL);
+        URLConnection conn = url.openConnection();
+
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+        }
+        return inputStream;
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
     }
 }
